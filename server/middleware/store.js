6ed7c882e439fba559f050c107017fb2480b86ec
Will Germany <germany.william@gmail.com>
2018-07-19 21:47:33 -0400

    Fixed middleware error

--- server/middleware/store.js
+++ server/middleware/store.js
@@ -3,10 +3,11 @@ import config from '../config';
 
 const db = config.db.url;
 
-const store = async () => {
+const store = () => {
     try {
-        await mongoose.connect(db, { useNewUrlParser: true });
-        console.log('Successfully connected to the database');
+        mongoose.connect(db, { useNewUrlParser: true })
+            .then(() => console.log('Successfully connected to the database'));
+
         return (req, res, next) => next();
     } catch (error) {
         console.log(error);
