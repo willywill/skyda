535ef0f6ff92f24c2137f75302e8bfe91a6e4558
Will Germany <germany.william@gmail.com>
2018-07-21 15:15:02 -0400

    Using ramda

--- server/mail/index.js
+++ server/mail/index.js
@@ -1,13 +1,23 @@
+import { head, length } from 'ramda';
 import { transport, mailOptions } from './transporter';
+import { log } from '../middleware/logger';
+
+const getAcceptedEmails = info => info.accepted;
+const getSentTo = info => head(getAcceptedEmails(info));
+const emailSent = info => length(getAcceptedEmails(info));
 
 const sendMail = options => {
     return new Promise((resolve, reject) => {
-        transport.sendMail(options, (error) => {
+        transport.sendMail(options, (error, info) => {
             if (error) {
                 reject(error);
             }
+
+            if (emailSent(info)) {
+                log.info(`Email was sent! Sent to - ${getSentTo(info)}`);
+                resolve();
+            }
         });
-        resolve();
     });
 };
 
