215cc1d1bedba573ad6c11a49d1c4164831824db
Will Germany <germany.william@gmail.com>
2018-07-21 19:19:18 -0400

    Initial two-factor functions

---
+++ server/two-factor/index.js
@@ -0,0 +1,36 @@
+import nexmo from 'nexmo';
+import config from '../config';
+
+const options = {
+    code_length: 6,
+    brand: config.appName,
+};
+
+export const sendVerifyText = number => new Promise((resolve, reject) => {
+    nexmo.verify.request({ number, ...options }, (error, result) => {
+        if (error) {
+            reject(error);
+        } else {
+            const requestId = result.request_id;
+            if (result.status === '0') {
+                resolve(requestId);
+            } else {
+                reject(result.error_text);
+            }
+        }
+    });
+});
+
+export const confirmVerifyText = (requestId, code) => new Promise((resolve, reject) => {
+    nexmo.verify.check({ request_id: requestId, code }, (error, result) => {
+        if (error) {
+            reject(error);
+        } else {
+            if (result && result.status === '0') {
+                resolve();
+            } else {
+                reject(result.error_text);
+            }
+        }
+    });
+});
