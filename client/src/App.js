b303aa26c170367dcefdc2a4a9f91cbe729770c8
Will Germany <germany.william@gmail.com>
2018-08-04 18:13:43 -0400

    Adding react w/ styled components

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React, { PureComponent } from 'react';
+import { Flex } from 'reflexbox';
+
+class App extends PureComponent {
+    render() {
+        return <Flex>{'Hello World!'}</Flex>;
+    }
+}
+
+export default App;
